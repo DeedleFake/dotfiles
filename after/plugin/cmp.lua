65b7df93f9288bd02cdd41a5cd4cae7f7b27af3d
DeedleFake <deedlefake@users.noreply.github.com>
2024-09-18 14:16:33 -0400

    reconfigure selection behavior for cmp

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -6,8 +6,11 @@ cmp.setup {
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = false }),
+		['<CR>'] = cmp.mapping.confirm { select = false },
+		['<Tab>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+		['<S-Tab>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
 	},
+
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
 	}, {
