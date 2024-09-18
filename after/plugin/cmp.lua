8dc204a296e4daaed9aad2f475b3535d71cc5296
DeedleFake <deedlefake@users.noreply.github.com>
2024-09-18 14:18:20 -0400

    change cmp abort keybind

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -5,7 +5,7 @@ cmp.setup {
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
+		['<C-c>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm { select = false },
 		['<Tab>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
 		['<S-Tab>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
