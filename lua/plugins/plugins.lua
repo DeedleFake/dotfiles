f4d1ea8d091940015d15c397366f25e44b417222
DeedleFake <deedlefake@users.noreply.github.com>
2025-01-08 20:45:45 -0500

    disable formatter plugins
    It's possible that they're causing problems for the LSPs.

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -70,30 +70,30 @@ return {
 		},
 	},
 
-	{
-		'mhartington/formatter.nvim',
-		cmd = { 'Format', 'FormatWrite' },
-		dependencies = {
-			'williamboman/mason.nvim',
-			'williamboman/mason-lspconfig.nvim',
-		},
-		config = function()
-			require('formatter').setup {
-				filetype = {
-					markdown = { require('formatter.filetypes.markdown').prettier },
-					eruby = {
-						function()
-							local config = require('formatter.filetypes.eruby').erbformatter()
-							config.exe = 'erb-format'
-							return config
-						end,
-					},
-					html = { require('formatter.filetypes.html').prettier },
-					css = { require('formatter.filetypes.css').prettier },
-				},
-			}
-		end,
-	},
+	--{
+	--	'mhartington/formatter.nvim',
+	--	cmd = { 'Format', 'FormatWrite' },
+	--	dependencies = {
+	--		'williamboman/mason.nvim',
+	--		'williamboman/mason-lspconfig.nvim',
+	--	},
+	--	config = function()
+	--		require('formatter').setup {
+	--			filetype = {
+	--				markdown = { require('formatter.filetypes.markdown').prettier },
+	--				eruby = {
+	--					function()
+	--						local config = require('formatter.filetypes.eruby').erbformatter()
+	--						config.exe = 'erb-format'
+	--						return config
+	--					end,
+	--				},
+	--				html = { require('formatter.filetypes.html').prettier },
+	--				css = { require('formatter.filetypes.css').prettier },
+	--			},
+	--		}
+	--	end,
+	--},
 
 	{
 		'neovim/nvim-lspconfig',
