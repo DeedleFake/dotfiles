a2b323a3c4db29b7dde3311f8ea84e434a41edc2
DeedleFake <deedlefake@users.noreply.github.com>
2025-01-20 17:55:22 -0500

    fix nvim-treesitter config

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -47,27 +47,29 @@ return {
 			vim.cmd('TSInstall all')
 			vim.cmd('TSUpdate all')
 		end,
-		opts = {
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-			auto_install = true,
+		config = function()
+			require('nvim-treesitter.configs').setup {
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+				auto_install = true,
 
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
 
-			indent = { enable = true },
+				indent = { enable = true },
 
-			--incremental_selection = {
-			--	enable = true,
-			--	keymaps = {
-			--		init_selection = "gnn", -- set to `false` to disable one of the mappings
-			--		node_incremental = "grn",
-			--		scope_incremental = "grc",
-			--		node_decremental = "grm",
-			--	},
-			--},
-		},
+				--incremental_selection = {
+				--	enable = true,
+				--	keymaps = {
+				--		init_selection = "gnn", -- set to `false` to disable one of the mappings
+				--		node_incremental = "grn",
+				--		scope_incremental = "grc",
+				--		node_decremental = "grm",
+				--	},
+				--},
+			}
+		end,
 	},
 
 	{
