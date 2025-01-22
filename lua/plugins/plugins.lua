0fef9e930636807601aeb2449ccb74935d01381c
DeedleFake <deedlefake@users.noreply.github.com>
2025-01-22 16:27:46 -0500

    disable Treesitter indentation
    Apparently it's just horribly broken in quite a few different
    languages and has been for years. Bizarre.

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -57,7 +57,7 @@ return {
 					additional_vim_regex_highlighting = false,
 				},
 
-				indent = { enable = true },
+				indent = { enable = false },
 
 				--incremental_selection = {
 				--	enable = true,
