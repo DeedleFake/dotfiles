c15e4d2f954afbc90e550b49d135f08514644803
DeedleFake <deedlefake@users.noreply.github.com>
2025-02-28 00:18:34 -0500

    add `$HOME/.local/bin` to `$PATH`

--- config.fish
+++ config.fish
@@ -1,9 +1,11 @@
-function fish_title
-    echo (prompt_pwd)
-end
+set -xp --path PATH "$HOME/.local/bin"
 
 alias cp "cp -i"
 alias df "df -h"
 alias free "free -m"
 alias ls "ls --color=auto"
 alias grep "grep --color=auto"
+
+function fish_title
+    echo (prompt_pwd)
+end
