7ae89947ef8dfa6451e4d5b4263efddf7539f07e
DeedleFake <deedlefake@users.noreply.github.com>
2025-02-27 23:44:31 -0500

    remove some unnecessary checks for plugin

--- config.fish
+++ config.fish
@@ -7,14 +7,3 @@ alias df "df -h"
 alias free "free -m"
 alias ls "ls --color=auto"
 alias grep "grep --color=auto"
-
-set PLUGIN_DIR /usr/share/fish/vendor_functions.d
-if test -f $PLUGIN_DIR/fish-syntax-highlighting.fish
-    source $PLUGIN_DIR/fish-syntax-highlighting.fish
-end
-if test -f $PLUGIN_DIR/fish-history-substring-search.fish
-    source $PLUGIN_DIR/fish-history-substring-search.fish
-end
-if test -f $PLUGIN_DIR/fish-autosuggestions.fish
-    source $PLUGIN_DIR/fish-autosuggestions.fish
-end
