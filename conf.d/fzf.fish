28ed9aa5b5c970bebeb80a3b62fb1aa7a98294fa
DeedleFake <deedlefake@users.noreply.github.com>
2025-02-28 00:44:43 -0500

    clean up the FZF configuration a tad

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -2,10 +2,16 @@ if type -q fzf
   fzf --fish | source
 end
 
-set FZF_DEFAULT_OPTS "--height=80% --margin=5% --border --layout=reverse --info=inline"
+set FZF_DEFAULT_OPTS \
+	--height 80% \
+	--margin 5% \
+	--border \
+	--layout reverse \
+	--info=inline
 
-set fzf_preview "$(status dirname)/../scripts/fzf-preview"
+set -l fzf_preview "$(status dirname)/../scripts/fzf-preview"
 if test -x "$fzf_preview"
-  set -a FZF_DEFAULT_OPTS "--preview='$fzf_preview {}' --preview-window=down"
+  set -a FZF_DEFAULT_OPTS \
+		--preview "'$fzf_preview {}'" \
+		--preview-window down
 end
-set --erase fzf_preview
