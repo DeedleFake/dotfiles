b5a7716caf2f82899afc71ea4caa402d6f38d242
DeedleFake <deedlefake@users.noreply.github.com>
2025-02-27 20:17:25 -0500

    better powerline-go configuration

--- conf.d/powerline.fish
+++ conf.d/powerline.fish
@@ -1,11 +1,23 @@
 if type -q powerline-go
 	function fish_prompt
-			powerline-go \
-				-error $status \
-				-jobs (count (jobs -p)) \
-				-cwd-mode plain \
-				-hostname-only-if-ssh \
-				-git-mode compact \
-				-newline
+		powerline-go \
+			-error $status \
+			-jobs (count (jobs -p)) \
+			-cwd-mode plain \
+			-hostname-only-if-ssh \
+			-git-mode compact \
+			-mode compatible \
+			-modules user,host,ssh,cwd,perms,git,jobs,root \
+			-shell bare
+	end
+
+	function fish_right_prompt
+		powerline-go \
+			-modules exit,duration \
+			-duration (math -s6 "$CMD_DURATION / 1000") \
+			-error $status \
+			-numeric-exit-codes \
+			-duration-low-precision \
+			-shell bare
 	end
 end
