f1156e6044b65eac4ac91a607402772cefd16e75
DeedleFake <deedlefake@users.noreply.github.com>
2026-05-20 12:30:23 -0400

    remove lazy loading for colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,10 +1,10 @@
 return {
 	-- { 'junegunn/seoul256.vim',              lazy = true, priority = 1000 },
-	{ 'shaunsingh/seoul256.nvim',           lazy = true },
-	{ 'tpope/vim-vividchalk',               lazy = true },
-	{ 'kristijanhusak/vim-hybrid-material', lazy = true },
-	{ 'hzchirs/vim-material',               lazy = true },
-	{ 'NLKNguyen/papercolor-theme',         lazy = true },
-	{ 'morhetz/gruvbox',                    lazy = true },
-	{ 'ember-theme/nvim',                   lazy = true, name = "ember" },
+	{ 'shaunsingh/seoul256.nvim' },
+	{ 'tpope/vim-vividchalk' },
+	{ 'kristijanhusak/vim-hybrid-material' },
+	{ 'hzchirs/vim-material' },
+	{ 'NLKNguyen/papercolor-theme' },
+	{ 'morhetz/gruvbox' },
+	{ 'ember-theme/nvim', name = "ember" },
 }
